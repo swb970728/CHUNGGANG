540283e8451d26c7fcaaee58849094bc89ba47ec
unknown <>
2017-04-27 18:37:43 +0900

    asd

---
+++ Class2.py
@@ -0,0 +1,15 @@
+class Wonbin:
+    def get_String(self):
+        self.a=input()
+        print(self.a)
+        
+    def print_String(self):
+        self.k = self.a
+        result = self.k.upper()
+        print(result)
+        
+        
+pey=Wonbin()
+pey.get_String()
+pey.print_String()
+        
